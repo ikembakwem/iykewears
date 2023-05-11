5a29d60e52a4fe6ee52f1d1b7a4cb6da2d11eeeb
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2023-05-11 14:07:45 +0100

    Created authenticate method in REST datasource

--- src/app/model/rest.datasource.ts
+++ src/app/model/rest.datasource.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { Product } from './product.model';
 import { Order } from './order.model';
 
@@ -10,6 +10,7 @@ const PORT = 4000;
 @Injectable()
 export class RestDataSource {
   baseUrl: string;
+  auth_token?: string;
 
   constructor(private http: HttpClient) {
     this.baseUrl = `${PROTOCOL}://${location.hostname}:${PORT}/`;
@@ -18,7 +19,22 @@ export class RestDataSource {
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.baseUrl + 'products');
   }
+
   saveOrder(order: Order): Observable<Order> {
     return this.http.post<Order>(this.baseUrl + 'orders', order);
+  }
+
+  authenticate(user: string, pass: string): Observable<boolean> {
+    return this.http
+      .post<any>(this.baseUrl + 'login', {
+        name: user,
+        password: pass,
+      })
+      .pipe(
+        map((response) => {
+          this.auth_token = response.success ? response.token : null;
+          return response.success;
+        })
+      );
   }
 }
