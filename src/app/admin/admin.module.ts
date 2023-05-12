8e45c23ce01f3baf4e9b2f6fb4fb324c7984220e
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2023-05-12 09:40:51 +0100

    Built the foundation of the main admin site

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -7,17 +7,38 @@ import { MaterialFeatures } from './material.module';
 import { AuthComponent } from './auth.component';
 import { AdminComponent } from './admin.component';
 import { AuthGuard } from './auth.guard';
+import { ProductTableComponent } from './productTable..component';
+import { ProductEditorComponent } from './productEditor.component';
+import { OrderTableComponent } from './orderTable.component';
+
+let mainRouting = {
+  path: 'main',
+  component: AdminComponent,
+  canActivate: [AuthGuard],
+  children: [
+    { path: 'products/:mode/:id', component: ProductEditorComponent },
+    { path: 'products/:mode', component: ProductEditorComponent },
+    { path: 'products', component: ProductTableComponent },
+    { path: 'orders', component: OrderTableComponent },
+    { path: '**', redirectTo: 'products' },
+  ],
+};
 
 let routing = RouterModule.forChild([
   { path: 'auth', component: AuthComponent },
-  { path: 'main', component: AdminComponent },
-  { path: 'main', component: AdminComponent, canActivate: [AuthGuard] },
+  mainRouting,
   { path: '**', redirectTo: 'auth' },
 ]);
 
 @NgModule({
   imports: [CommonModule, FormsModule, routing, MaterialFeatures],
-  declarations: [AuthComponent, AdminComponent],
+  declarations: [
+    AuthComponent,
+    AdminComponent,
+    ProductTableComponent,
+    ProductEditorComponent,
+    OrderTableComponent,
+  ],
   providers: [AuthGuard],
 })
 export class AdminModule {}
--- src/app/admin/orderTable.component.ts
+++ src/app/admin/orderTable.component.ts
@@ -3,4 +3,4 @@ import { Component } from '@angular/core';
 @Component({
   template: `<h3 style="padding: 10px">Order Table placeholder</h3>`,
 })
-export class OrderTable {}
+export class OrderTableComponent {}
