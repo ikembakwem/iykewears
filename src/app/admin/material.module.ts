3b1f2c4662a81a360c6750302d287ddee636674b
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2023-05-12 08:50:07 +0100

    Improved the admin main page

--- src/app/admin/material.module.ts
+++ src/app/admin/material.module.ts
@@ -1,6 +1,17 @@
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
-const features: any[] = [];
+const features: any[] = [
+  MatToolbarModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatDividerModule,
+  MatButtonModule,
+];
 
 @NgModule({
   imports: [features],
